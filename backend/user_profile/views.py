860afa46122055013622b1aed308c21da214e85c
Fabian N <fabogz281@gmail.com>
2025-04-14 20:12:39 -0600

    Profile, AccountManagment y base de notificaciones funcional

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from firebase_admin import firestore
 from firebase_admin import auth as firebase_auth
-from .permissions import FirebaseAuthentication  # Importa el permiso personalizado
+from .permissions import FirebaseAuthentication  #
 from datetime import datetime
 
 class ProfileView(APIView):
